f30a297c5089755922dd6b5a1def78ae6758a057
JuanRaV <juanravirgen@gmail.com>
2023-06-07 19:56:17 -0600

    Finishing main page

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,47 @@
-const inicio = (req,res)=>{
+import {Precio,Categoria,Propiedad} from '../models/Index.js'
+
+const inicio = async(req,res)=>{
+    const [categorias,precios,casas,departamentos] = await Promise.all([
+        Categoria.findAll({raw:true}),
+        Precio.findAll({raw:true}),
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId:1
+            },
+            include:[
+                {
+                    model:Precio,
+                    as:'precio'
+                }
+            ],
+            order:[
+                ['createdAt', 'DESC']
+            ]
+        }),
+        Propiedad.findAll({
+            limit:3,
+            where:{
+                categoriaId:2
+            },
+            include:[
+                {
+                    model:Precio,
+                    as:'precio'
+                }
+            ],
+            order:[
+                ['createdAt', 'DESC']
+            ]
+        })
+    ])
+
     res.render('inicio',{
-        pagina:'Inicio'
+        pagina:'Inicio',
+        categorias,
+        precios,
+        casas,
+        departamentos
     })
 }
 const categoria = (req,res)=>{
